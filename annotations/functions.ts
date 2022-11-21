6b0e6a210aafa0825bf5091ca07fd290d8816009
Hisham Paloli <hishampaloli786@gmail.com>
2022-11-21 11:40:55 +0530

    anotations -functions

---
+++ annotations/functions.ts
@@ -0,0 +1,37 @@
+const add = (a: number, b: number): number => {
+    return a + b ;
+};
+
+
+const sub = (a: number, b:number)  => {
+   return a - b;
+}
+
+function divide(a:number, b:number): number  {
+    return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+    return a * b;
+}
+
+const logger = (message: string): void => {
+      console.log(message);
+}
+
+const throwError = (message: string): never => {
+    throw new Error(message);
+}
+
+const forecast = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date);
+    console.log(weather);
+    
+}
+
+logWeather(forecast)
--- annotations/variables.ts
+++ annotations/variables.ts
@@ -27,8 +27,34 @@ let point: { x: number; y: number } = {
 
 //Function
 
-const logNumber: (i: number) => void = (i: number) => {
+const logNumber: (i: number | string) => void = (i: number | string) => {
   console.log(i);
 };
 
+//when to use annotation
+// 1) Funtion that return the 'any' type;
 
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+
+let word = ["red", "gtre", "blue"];
+let foundWord: boolean; // declared
+
+for (let i = 0; i < word.length; i++) {
+  if (word[i] === "red") {
+    foundWord = true; // initialized
+  }
+}
+
+// 3)Variable whose type cannot be inferred correctly;
+let numbers = [-10, 1, -43];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}

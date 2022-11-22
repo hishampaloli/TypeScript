77e8ddfde5a24122741f06061a20abcd140118f5
Hisham Paloli <hishampaloli786@gmail.com>
2022-11-22 17:10:10 +0530

    ts

---
+++ annotations/Enums.ts
@@ -0,0 +1,12 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+enum SeatChoice {
+  AISLE = 'aisle',
+  MIDDLE = 3,
+  WINDOW,
+}
+
+const hcSeat = SeatChoice.AISLE
+const mSeat = SeatChoice.WINDOW

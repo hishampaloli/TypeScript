45748cfdb563c4e256f851f7aee748fe10ea07ab
Hisham Paloli <hishampaloli786@gmail.com>
2022-11-22 14:13:42 +0530

    genericsContrains

--- annotations/generics.ts
+++ annotations/generics.ts
@@ -45,3 +45,29 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything<number>([4]);
+
+// Generic contrains
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+    print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].print();
+    }
+}
+
+printHousesOrCars([new House(), new Car()])

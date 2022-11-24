c86c7be2a8d499ae155d6d8ce0f08c99c0582d85
Hisham Paloli <hishampaloli786@gmail.com>
2022-11-24 14:15:07 +0530

    decoratorsExpress

--- annotations/decorators.ts
+++ annotations/decorators.ts
@@ -1,21 +1,49 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = "red";
 
+  @testDecorator
   get formattedColor(): string {
     return `This is red in color${this.color}}`;
   }
 
-
-  @logError  
-  pilot(): void {
-    throw new Error();
-    console.log('swish');
-    
+  @logError("Oops, boat sunk!")
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === "fast") {
+      console.log("swish");
+    } else {
+      console.log("nothing");
+    }
   }
 }
 
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  console.log("Target:", target);
-  console.log("key:", key);
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  // console.log(target);
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
---
+++ annotations/metadata.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+
+@controller
+class Plane {
+  color: string = "red";
+
+  @get("/login")
+  fly(): void {
+    console.log("vrrrrrr");
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("path", path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    console.log(Reflect.getMetadata("path", target.prototype, key));
+  }
+}
+
+// console.log(Reflect.getMetadata("secret", Plane.prototype, "fly"));
